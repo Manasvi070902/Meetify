7f1cb11f92b4a73c248348cdeb142f9c36e170bc
Manasvi070902 <manasvi.alimchandani2019@vitstudent.ac.in>
2021-06-21 01:57:28 +0530

    server join room added

--- server/controllers/meets/meets.js
+++ server/controllers/meets/meets.js
@@ -17,4 +17,22 @@ exports.createMeet = async({ roomID, hostID }) => {
         console.log(err)
     }
 }
-
+exports.addMember = async({ roomID, userID }) => {
+    try{
+        await Promise.all([
+            User.findByIdAndUpdate(userID, {
+                $addToSet: {
+                    'meets': roomID
+                }
+            }),
+            Meet.findByIdAndUpdate(roomID, {
+                $addToSet: {
+                    'members': userID
+                }
+            })
+        ])
+    }
+    catch(err){
+        console.log(err)
+    }
+}
--- server/controllers/webSockets.js
+++ server/controllers/webSockets.js
@@ -3,7 +3,7 @@ const http = require('http')
 const uuid = require('uuid')
 const getTokenDetails = require("../middleware/verifytoken");
 
-const { createMeet} = require('./meets/meets');
+const { createMeet , addMember} = require('./meets/meets');
 
 const webSockets = app => {
   
@@ -34,6 +34,52 @@ const webSockets = app => {
             catch(err){
                 console.log(err)
                 if(err.name === "LoginError"){
+                    socket.emit("unauthorized", "Please login again")
+                    return
+                }
+                socket.emit("something broke", "Something went wrong, please try again!")
+            }
+        })
+        socket.on("join room", async({ roomID, token }) => {
+            try{
+                const { id, name } = await getTokenDetails(token)
+                if(!uuid.validate(roomID)){
+                    socket.emit("invalid room")
+                    return;
+                }
+                const roomData = io.sockets.adapter.rooms[roomID]
+                if(!roomData){
+                    socket.emit("invalid room")
+                    return;
+                }
+                if(roomData.length > 100){
+                    socket.emit("room full")
+                    return;
+                }
+                socket.roomID = roomID
+                socket.userName = name
+                const usersInThisRoom1 = []
+                for(let key in roomData.sockets){
+                    console.log(io.sockets.connected[key].userName)
+                    console.log(key.userName)
+                    usersInThisRoom1.push({
+                        id: key,
+                        username: io.sockets.connected[key].userName
+                    })
+                }
+                const usersInThisRoom = Object.keys(roomData.sockets) 
+                socket.join(roomID)
+                console.log("all members", usersInThisRoom)
+                io.to(socket.id).emit("all members", usersInThisRoom1);
+                addMember({
+                    roomID,
+                    userID: id
+                })
+                console.log("join room",roomID, socket.id)
+            }
+            catch(err){
+                console.log(err)
+                if(err.name === "JsonWebTokenError"){
                     socket.emit("unauthorized", "Please login again")
                     return
                 }
