1c365a6ab521db7454074d791394712dbdbed563
Manasvi070902 <manasvi.alimchandani2019@vitstudent.ac.in>
2021-07-09 04:46:02 +0530

    home ui

--- react-app/src/Views/ChatPage/SideBar/index.js
+++ react-app/src/Views/ChatPage/SideBar/index.js
@@ -11,59 +11,58 @@ import './sidebar.css'
 
 
 const Sidebar = (props) => {
-    const {auth} = props;
- const [meets, setMeets] = useState([]);
+  const { auth } = props;
+  const [meets, setMeets] = useState([]);
 
- 
- useEffect(() => {
+
+  useEffect(() => {
     const fetchMeets = async () => {
-        var meets = []
-      const response = await axios.get(`http://localhost:5000/meets/`,{
-        headers: {'auth_id' : auth.uid}
+      var meets = []
+      const response = await axios.get(`http://localhost:5000/meets/`, {
+        headers: { 'auth_id': auth.uid }
       })
       const meet = response.data.meets;
-      setMeets(meet)    
+      setMeets(meet)
     };
 
     fetchMeets().catch((error) => {
-   console.log(error.message)
-    
-    });
-   
-  }, []);
-    
-    return (
-            <>
-                {meets.length>0 && meets.map(meet => {
-                        console.log(meet)     
-                return (
-                  <> 
-                <div className="sidebar__header mt-4">
-                    <div className="sidebar__headerRight">
-                    <h4>Meetings</h4>
-                    </div>
-                </div>    
+      console.log(error.message)
 
-                
-                    <div className="sidebar__chats">
-                    <SideChat key={meet._id} id={meet._id} name={meet.name}/>
-                    </div>
-                  </>
-                     ) })}
-                     
-                
-               
-            
-         </>
-    )
+    });
+
+  }, []);
+
+  return (
+    <>
+
+      {meets.length > 0 &&
+        <div className="sidebar__header mt-4">
+          <div className="sidebar__headerRight">
+            <h4>Meetings</h4>
+          </div>
+        </div>}
+
+      {meets.length > 0 && meets.map(meet => {
+        return (
+          <div className="sidebar__chats">
+            <SideChat key={meet._id} id={meet._id} name={meet.name} />
+          </div>
+
+        )
+      })}
+
+
+
+
+    </>
+  )
 }
 
-const mapStateToProps = ({auth, firebase}) => {
-    return {
-      auth: firebase.auth,
-      authError: auth.authError
-    }
+const mapStateToProps = ({ auth, firebase }) => {
+  return {
+    auth: firebase.auth,
+    authError: auth.authError
   }
-  export default connect(mapStateToProps)(Sidebar)
-  
-  
+}
+export default connect(mapStateToProps)(Sidebar)
+
--- react-app/src/Views/HomePage/index.js
+++ react-app/src/Views/HomePage/index.js
@@ -1,80 +1,95 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { useStyles } from '../../Utils/globalStyles'
-import  {Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import TextField from '@material-ui/core/TextField';
-
-
+import homepic from "../../Assets/home.svg"
 
 
 
 const HomePage = (props) => {
-const [teams , setTeams] = useState([])
-const [value, setValue] = useState();
-const [meetname, setMeetName] = useState('');
+  const [value, setValue] = useState();
+  const [meetname, setMeetName] = useState('');
 
-const handleChange = (event) => {
-  setValue(event.target.value);
-};
-const handleMeetName = (event) => {
-  setMeetName(event.target.value);
-};
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+  const handleMeetName = (event) => {
+    setMeetName(event.target.value);
+  };
   const classes = useStyles();
-  const {auth} = props;
-  if(!auth.uid && auth.isLoaded){
+  const { auth } = props;
+  if (!auth.uid && auth.isLoaded) {
     return <Redirect to="/login" />
   }
-  const startMeet = async() => {
-    localStorage.setItem('meetname' , meetname)
-      window.open(`/room?host=${true}`)
-  }
-  const joinpreviewMeet = async() => {
-    window.open(`/room?room=${value}`)
-}
-
-    return (
-      <main className={classes.content}>
-          <div className={classes.toolbar} />
-       
-        <p className="lead d-flex justify-content-center">Hi &nbsp; <span className="text-primary"> {auth.displayName}</span>!</p>
-          <p className="lead d-flex justify-content-center">Welcome to MS Teams.</p>
-          <div className="d-flex justify-content-center">
-          <TextField
-          id="standard-multiline-flexible"
-          label="Enter Meet Name"
-          multiline
-          rowsMax={4}
-          value={meetname}
-          onChange={handleMeetName}
-          color="secondary"
-        />
-          <Button className="p-2 m-5 "  variant="info"  onClick={startMeet} >Create Meet</Button> <br /></div>
-          <form className="d-flex justify-content-center" noValidate autoComplete="off">
-          <TextField
-          id="standard-multiline-flexible"
-          label="Enter Code"
-          multiline
-          rowsMax={4}
-          value={value}
-          onChange={handleChange}
-          color="secondary"
-        />
-        <Button className="p-2 m-1"  variant="danger" onClick = {joinpreviewMeet} >Join Meet </Button>
-        </form>
-      
-        </main>
-      
-    )
-  }
-  const mapStateToProps = ({auth, firebase}) => {
-    return {
-      auth: firebase.auth,
-      authError: auth.authError
+  const startMeet = async () => {
+    localStorage.setItem('meetname', meetname)
+    if(meetname === ""){
+      alert("Enter Meetname")
+    }else{
+    window.open(`/room?host=${true}`)
     }
   }
-  export default connect(mapStateToProps)(HomePage)
-  
+  const joinpreviewMeet = async () => {
+    window.open(`${value}`)
+  }
 
- 
+  return (
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+
+
+      <div className="container-fluid d-lg-flex  d-md-flex justify-content-center p-lg-5 ">
+
+        <div className="container col-12 col-lg-7" style={{ marginTop: "20vh" }} >
+          <h4 className=" m-3 justify-content-center">Hi &nbsp;<span style={{ color: "#2dbdc3" }}> {auth.displayName}</span></h4>
+          <h6 className=" m-3 justify-content-center" style={{ fontSize: "1.4rem" }}>Connect,collaborate,and celebrate from anywhere with Meetify!</h6>
+          <div className="row container m-3">
+            <div className=" justify-content-center m-3">
+              <TextField
+                id="meetname"
+                label="Enter Meet Name"
+                multiline
+                rowsMax={4}
+                value={meetname}
+                onChange={handleMeetName}
+                color="primary"
+              />
+              <Button className="p-2 m-1 " variant="info" onClick={startMeet} >Create Meet</Button> <br />
+            </div>
+            <form className=" justify-content-center m-3" noValidate autoComplete="off">
+              <TextField
+                id="meeturl"
+                label="Enter Meet URL"
+                multiline
+                rowsMax={4}
+                value={value}
+                onChange={handleChange}
+                color="secondary"
+              />
+              <Button className="p-2 m-1" variant="danger" onClick={joinpreviewMeet} >Join Meet </Button>
+            </form>
+          </div>
+        </div>
+        <div className="col-12 mt-4  col-lg-5">
+          <img className="img-fluid rounded d-block float-right" src={homepic} alt="pic" />
+        </div>
+      </div>
+
+
+    </main>
+
+  )
+}
+const mapStateToProps = ({ auth, firebase }) => {
+  return {
+    auth: firebase.auth,
+    authError: auth.authError
+  }
+}
+export default connect(mapStateToProps)(HomePage)
+
+
+
 
